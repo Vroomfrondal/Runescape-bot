a69f5ebd0e68d917cfb67869ca352226b82461a9
Christopher DeLeon <lionscantcode@gmail.com>
2021-12-07 20:04:34 -0600

    Update index.js
    - added sleep() function to delay starting / stopping of program
    
    - added moveMouse and mouseClick functions to find tree based on pixels in game, and click the tree to start chopping down

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // import the robotjs library
 let robot = require('robotjs') //
 
-
 function main() {
 
-    // Move the mouse across the screen top left corner
-    console.log("Starting...")
-    robot.moveMouse(0, 0)
+    // Sleep function to start script
+    console.log("Starting the magic...")
+    sleep(4000)
+    
+    // Move mouse + click to location of tree
+    robot.moveMouse(1090, 696)
+    robot.mouseClick()
+
     console.log("Done.")
+
 }
 
+function sleep(ms) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+  }
+
  main()
-
-
-
-
