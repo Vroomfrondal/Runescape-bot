aef0cef9e7d2d2f1b75f1de7d0ce44535805d349
Christopher DeLeon <lionscantcode@gmail.com>
2021-12-08 16:13:20 -0600

    Update index.js
    - removed pixel-finding tree algorithm.
    
    - Implemented image-finding tree algorithm
    
    - added if loop to check if tree is in sight
    
    - added find tree function based on hex-values
    
    -

--- index.js
+++ index.js
@@ -5,27 +5,37 @@ function main() {
     console.log("Starting Up...")
     sleep(4000)
 
-    let first_tree_x = 840
-    let first_tree_y = 653
-    let second_tree_x = 1714
-    let second_tree_y = 844
-
     // a bad bad infinate loop
     while (true) {   
+        let tree = findTree()
+        // if no tree can be found, don't execute
+        if (tree == false) {
+            console.log("No Tree in sight.")
+            break
+        }
 
-        //chop down first tree
-        robot.moveMouse(first_tree_x, first_tree_y)
-        robot.mouseClick()
-        sleep(4000)
-        dropLogs()
-
-        //chop down second tree
-        robot.moveMouse(second_tree_x, second_tree_y)
+        //chop down tree found in image screenshot function
+        robot.moveMouse(tree.x, tree.y)
         robot.mouseClick()
         sleep(4000)
         dropLogs()
     }
 }
+
+function findTree() {
+    let x = 300, y = 300, width = 1300, height = 400
+    let img = robot.screen.capture(x, y, width, height) //1440p screen
+
+    //array for tree color hex values
+    let tree_colors = ["735835", "765B37", "664E2E", "7A5D39", "5B462A", "705634", "80623A"]
+
+    for (var i = 0; i < 100; i ++ ) { // iterator loop > less than 100 > increase i by 1
+        let random_x =
+        let random_y = 
+        let sample_color = img.colorAt(random_x, random_y)
+
+}
+
 
 function dropLogs() {
     let inventory_location_x = 2393
@@ -50,4 +60,10 @@ function sleep(ms) {
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
   }
 
-//main()
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+main()
