d55509dde60a72a178a3c072133985559aed88d3
Christopher DeLeon <lionscantcode@gmail.com>
2021-12-07 18:12:36 -0600

    Update Index.js
    - Added node modules
    
    - added test file for robotjs NPM library which moves mouse in a sine wave across screen

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // import the robotjs library
 
-let robot = require('robotjs')
+//let robot = require('robotjs')
 
-console.log("Starting.....")
-console.log("Done.")
+console.log("Starting...")
 
-robot.moveMouse(0, 0)
+// Move the mouse across the screen as a sine wave.
+let robot = require("robotjs");
+ 
+// Speed up the mouse.
+robot.setMouseDelay(2);
+ 
+let twoPI = Math.PI * 2.0;
+let screenSize = robot.getScreenSize();
+let height = (screenSize.height / 2) - 10;
+let width = screenSize.width;
+ 
+for (let x = 0; x < width; x++)
+{
+    y = height * Math.sin((twoPI * x) / width) + height;
+    robot.moveMouse(x, y);
+}
 
