c613f0a2b914527d9f2931fd4e789064ac6595d1
Christopher DeLeon <lionscantcode@gmail.com>
2021-12-15 01:10:47 -0600

    Updated file typo
    - added loop for barbarian agility course

--- scripts/Agility/barbarian_course.js
+++ scripts/Agility/barbarian_course.js
@@ -1,28 +1,37 @@
 // import the robotjs library
 let robot = require('robotjs') //
 
+function main() {
+    for (let i = 0; i < 100; i ++) {
+        bank()
+    }
+}
+
 function bank() {
-    sleep(4000) //debug use
+    sleep(2000) //debug use
 
     //move character to banking location
     console.log("Crossing Rope Swing")
-    robot.moveMouse(1256, 933)
+    robot.moveMouse(1300, 859)
+    sleep(100)
     robot.mouseClick()
-    sleep(4000)
+    sleep(2500)
 
     console.log("Running over long log.")
     robot.moveMouse(1138, 1222)
+    sleep(200)
     robot.mouseClick()
-    sleep(5000)
+    sleep(9000)
 
     console.log("Going to net.")
     robot.moveMouse(778, 805)
+    sleep(200)
     robot.mouseClick()
-    sleep(3500)
+    sleep(2800)
 
     console.log("Crossing wall.")
     robot.moveMouse(1021, 573)
-    sleep(100)
+    sleep(200)
     robot.mouseClick()
     sleep(5000)
 
@@ -33,47 +42,44 @@ function bank() {
     sleep(2500)
 
     console.log("Going to next spot.")
-    robot.moveMouse(2490, 106)
+    robot.moveMouse(2493, 105)
     robot.mouseClick()
     sleep(3000)
 
 
     console.log("Crumbling wall 1.")
     robot.moveMouse(1360, 598)
+    sleep(200)
     robot.mouseClick()
-    sleep(3000)
+    sleep(3400)
 
     console.log("Crumbling wall 2.")
     robot.moveMouse(1684, 724)
+    sleep(200)
     robot.mouseClick()
     sleep(3000)
 
     console.log("Crumbling wall 3.")
     robot.moveMouse(1683, 713)
+    sleep(200)
     robot.mouseClick()
-    sleep(2000)
+    sleep(3000)
+
+    // bug fix
+    console.log("Clicking Ground.")
+    robot.moveMouse(1294, 731)
+    robot.mouseClick()
+    sleep(500)
 
     console.log("Completed lap.")
     robot.moveMouse(2507, 130)
+    sleep(200)
     robot.mouseClick()
-    sleep(2000)
+    sleep(3000)
 
 }
 
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
-bank()
+main()
 
 
 
